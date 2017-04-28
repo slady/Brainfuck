b69514532cb215a835065742a17d5ebbf8f8e6f4
Petr Sládek <petr.sladek@i.cz>
2017-04-28 19:28:17 +0200

    Brainfuck tests extended

--- src/test/java/ps/java/brainfuck/BrainfuckTest.java
+++ src/test/java/ps/java/brainfuck/BrainfuckTest.java
@@ -7,8 +7,18 @@ import static org.junit.Assert.*;
 public class BrainfuckTest {
 
     @Test
-    public void main() {
+    public void run8() {
         assertEquals("8", Brainfuck.run("++>+++++[<+>-]++++++++[<++++++>-]<.", ""));
+    }
+
+    @Test
+    public void runA() {
+        assertEquals("8", Brainfuck.run("++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>.", ""));
+    }
+
+    @Test
+    public void runB() {
+        assertEquals("8", Brainfuck.run(">+++++++++[<++++++++>-]<.>+++++++[<++++>-]<+.+++++++..+++.[-]>++++++++[<++++>-]<.#>+++++++++++[<+++++>-]<.>++++++++[<+++>-]<.+++.------.--------.[-]>++++++++[<++++>-]<+.[-]++++++++++.", ""));
     }
 
 }
