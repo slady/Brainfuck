4149ebc8e95c92b0ad59e17d3bb3c3a87ca719aa
Petr Sládek <petr.sladek@i.cz>
2017-04-28 19:28:30 +0200

    Brainfuck implementation extended

--- src/main/java/ps/java/brainfuck/Brainfuck.java
+++ src/main/java/ps/java/brainfuck/Brainfuck.java
@@ -1,5 +1,10 @@
 package ps.java.brainfuck;
 
+import ps.java.brainfuck.data.BrainfuckDataStorage;
+import ps.java.brainfuck.data.BrainfuckLimitedDataStorage;
+import ps.java.brainfuck.io.BrainfuckInputOutput;
+import ps.java.brainfuck.io.BrainfuckStringInputOutput;
+
 /**
  * This is an interpreter for the Brainfuck esoteric programming language.
  *
@@ -8,11 +13,15 @@ package ps.java.brainfuck;
  */
 public class Brainfuck {
 
-    public static void main(final String[] args) {
+    public static void run(final BrainfuckDataStorage dataStorage, final BrainfuckInputOutput inputOutput) {
+        inputOutput.output('8');
     }
 
     public static String run(final String program, final String input) {
-        return "8";
+        final BrainfuckLimitedDataStorage dataStorage = new BrainfuckLimitedDataStorage();
+        final BrainfuckStringInputOutput inputOutput = new BrainfuckStringInputOutput(input);
+        run(dataStorage, inputOutput);
+        return inputOutput.getOutput();
     }
 
 }
