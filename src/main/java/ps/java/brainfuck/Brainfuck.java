abafe4fc0a8d0190af981292f6d400c8e2b5a80e
Petr Sládek <petr.sladek@i.cz>
2017-04-28 19:33:24 +0200

    dummy parser class created

--- src/main/java/ps/java/brainfuck/Brainfuck.java
+++ src/main/java/ps/java/brainfuck/Brainfuck.java
@@ -4,6 +4,7 @@ import ps.java.brainfuck.data.BrainfuckDataStorage;
 import ps.java.brainfuck.data.BrainfuckLimitedDataStorage;
 import ps.java.brainfuck.io.BrainfuckInputOutput;
 import ps.java.brainfuck.io.BrainfuckStringInputOutput;
+import ps.java.brainfuck.parser.BrainfuckParser;
 
 /**
  * This is an interpreter for the Brainfuck esoteric programming language.
@@ -13,14 +14,15 @@ import ps.java.brainfuck.io.BrainfuckStringInputOutput;
  */
 public class Brainfuck {
 
-    public static void run(final BrainfuckDataStorage dataStorage, final BrainfuckInputOutput inputOutput) {
+    public static void run(final BrainfuckParser parser, final BrainfuckDataStorage dataStorage, final BrainfuckInputOutput inputOutput) {
         inputOutput.output('8');
     }
 
     public static String run(final String program, final String input) {
+        final BrainfuckParser parser = new BrainfuckParser();
         final BrainfuckLimitedDataStorage dataStorage = new BrainfuckLimitedDataStorage();
         final BrainfuckStringInputOutput inputOutput = new BrainfuckStringInputOutput(input);
-        run(dataStorage, inputOutput);
+        run(parser, dataStorage, inputOutput);
         return inputOutput.getOutput();
     }
 
---
+++ src/main/java/ps/java/brainfuck/parser/BrainfuckParser.java
@@ -0,0 +1,10 @@
+package ps.java.brainfuck.parser;
+
+/**
+ * This is the parser for the {@link ps.java.brainfuck.Brainfuck} programming language.
+ *
+ * @author slady@slady.net
+ */
+public class BrainfuckParser {
+
+}
